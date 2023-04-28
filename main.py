6ecbd86e8e0e9346003ce63c947de4bb50fa421a
Tarzan <maksymilian.l.bartosik@gmail.com>
2023-04-28 19:01:27 +0200

    added requirements, added plot, tried to improve generation

--- main.py
+++ main.py
@@ -2,23 +2,23 @@
 from src.Tree import Tree
 from src.Agent import Agent, Player
 
-# TODO: Turns, player, tradeposts
+# TODO: Turns, player, tradeposts, matplotlib i mplcursors
 
-p = Tree(3)
-
+p = Tree(10)
 p.show()
+p.plot()
 
 # creation of agent and player
-pa = Player(2, 0)
-
-pa.show()
-
-currentp = p.map[pa.position]
-possibleposition = currentp.connections
-dir = int(input())
-pa.updateposition(pa.nextposition(connections=possibleposition, direction=dir))
-
-pa.show()
+# pa = Player(2, 0)
+#
+# pa.show()
+#
+# currentp = p.map[pa.position]
+# possibleposition = currentp.connections
+# dir = int(input())
+# pa.updateposition(pa.nextposition(connections=possibleposition, direction=dir))
+#
+# pa.show()
 
 # basic movement of agent
 # for i in range(10):
--- src/Tree.py
+++ src/Tree.py
@@ -1,26 +1,52 @@
 import random
+from win32api import GetSystemMetrics
+import matplotlib.pyplot as plt
 
-#TODO naprawić wiszące wierzchołki, podgrafy, różne prędkosci na tej samej sciezce
+
+# TODO naprawić wiszące wierzchołki, podgrafy, różne prędkosci na tej samej sciezce
 class Point:
-    def __init__(self, id, connections, weights):
+    def __init__(self, id, connections, weights, x, y, z):
         self.id = id
         self.connections = connections
         self.weights = weights
+        self.x = x
+        self.y = y
+        self.z = z
 
     @staticmethod
-    def create(id, connections, weights):
-        return Point(id, connections, weights)
+    def create(id, connections, weights, x, y, z):
+        return Point(id, connections, weights, x, y, z)
+
 
 class Tree:
     def __init__(self, size):
         self.map = []
         for i in range(size):
-            rand = random.randrange(1, size)
-            connections = [random.randrange(0, size) for c in range(rand)]
-            weights = [random.random() for c in range(rand)]
-            nPoint = Point.create(i, connections, weights)
+            connections = [random.randrange(0, size) for c in range(4)]
+            weights = [random.random() for c in range(4)]
+            x = random.random()
+            y = random.random()
+            z = random.random()
+            nPoint = Point.create(i, connections, weights, x, y, z)
             self.map.append(nPoint)
 
     def show(self):
         for i in self.map:
-            print(i.id, i.connections, i.weights)
+            print(i.id, i.connections, i.weights, i.x, i.y, i.z)
+
+    def plot(self):
+        plt.rcParams['toolbar'] = 'None'
+        screen_width = GetSystemMetrics(0)
+        screen_height = GetSystemMetrics(1)
+        fig = plt.figure(figsize=(screen_width, screen_height), dpi=250)
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_axis_off()
+        fig.canvas.set_window_title('fullscreen_toggle')
+        plt.get_current_fig_manager().window.showMaximized()
+        for point in self.map:
+            ax.scatter(point.x, point.y, point.z)
+            for i, connection in enumerate(point.connections):
+                ax.plot([point.x, self.map[connection].x], [point.y, self.map[connection].y],
+                        [point.z, self.map[connection].z], color='black', alpha=0.5)
+        plt.tight_layout()
+        plt.show()
